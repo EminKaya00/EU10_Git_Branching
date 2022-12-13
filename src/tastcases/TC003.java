a01ca644d3febfa8f336f3821d40e9a0b30d5f76
Emin Kaya <kaya075@gmail.com>
2022-12-13 22:56:28 +0300

    last update tc003

--- src/tastcases/TC003.java
+++ src/tastcases/TC003.java
@@ -4,5 +4,6 @@ public class TC003 {
     public static void main(String[] args) {
         System.out.println("newest");
         System.out.println("line");
+        System.out.println("added");
     }
 }
